acc3e30de04781a27b5244d917e12b6cd7eaa0a9
diptasahacse <diptasaha.lpu.cse@gmail.com>
2021-11-22 04:13:26 +0600

    first update

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
-    
-        
-
          function setValue(){
              
 
 
             //Height And Width
-            document.getElementById("heightWidthValue").innerText = document.getElementById("heightWidthRangeCounter").value;
+           document.getElementById("heightWidthValue").innerText = document.getElementById("heightWidthRangeCounter").value;
+
             
 
            //  document.getElementById("makeBox").style.height = document.getElementById("heightWidthRangeCounter").value+"px";
@@ -16,12 +14,12 @@
 
 
            // Width Value
-           document.getElementById("widthValue").innerText = document.getElementById("heightRangeCounter").value;
-           document.getElementById("makeBox").style.height = document.getElementById("heightRangeCounter").value+"px";
+           document.getElementById("widthValue").innerText = document.getElementById("widthRangeCounter").value;
+           document.getElementById("makeBox").style.height = document.getElementById("widthRangeCounter").value+"px";
            
            // Height Value
-           document.getElementById("hightValue").innerText = document.getElementById("widthRangeCounter").value;
-           document.getElementById("makeBox").style.width = document.getElementById("widthRangeCounter").value+"px";
+           document.getElementById("hightValue").innerText = document.getElementById("heightRangeCounter").value;
+           document.getElementById("makeBox").style.height = document.getElementById("heightRangeCounter").value+"px";
            
            // Border Value
            document.getElementById("borderValue").innerText = document.getElementById("borderRangeCounter").value;
@@ -34,9 +32,26 @@
 
        };
        function change(parameter){
+        if(parameter === 'heightWidth'){
+            document.getElementById("heightWidthValue").innerText = document.getElementById("heightWidthRangeCounter").value;
+            //height
+            document.getElementById("hightValue").innerText = document.getElementById("heightWidthRangeCounter").value;
+            document.getElementById("makeBox").style.height = document.getElementById("heightWidthRangeCounter").value+"px";
+            //width
+            document.getElementById("widthValue").innerText = document.getElementById("heightWidthRangeCounter").value;
+            document.getElementById("makeBox").style.width = document.getElementById("heightWidthRangeCounter").value+"px";
+
+            //call 
+            change('heightwidth');
+        }
+            if(parameter === 'heightwidth'){
+                document.getElementById("heightRangeCounter").value = document.getElementById("heightWidthRangeCounter").value;
+                document.getElementById("widthRangeCounter").value = document.getElementById("heightWidthRangeCounter").value;
+                
+
+            }
            if(parameter === 'height'){
                document.getElementById("hightValue").innerText = document.getElementById("heightRangeCounter").value;
-
                document.getElementById("makeBox").style.height = document.getElementById("heightRangeCounter").value+"px";
            }
            if(parameter === 'width'){
