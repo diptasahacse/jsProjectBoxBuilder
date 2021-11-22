e5c448638f8deb19c6d8a89da1b27a4b56f05447
diptasahacse <diptasaha.lpu.cse@gmail.com>
2021-11-23 03:37:39 +0600

    Update design and code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
+        /* 
+        JavaScript Project
+        Author : Dipta Saha
+        Email : diptasaha.lpu.cse@gmail.com
+        Github : https://github.com/diptasahacse
+        Website : https://diptasaha.com/
+        */
+         
          function setValue(){
+             //backround
+             document.getElementById("makeBox").style.backgroundColor = 'rgb(74, 234, 1)';
              
 
 
@@ -33,6 +43,7 @@
        };
        function change(parameter){
         if(parameter === 'heightWidth'){
+            
             document.getElementById("heightWidthValue").innerText = document.getElementById("heightWidthRangeCounter").value;
             //height
             document.getElementById("hightValue").innerText = document.getElementById("heightWidthRangeCounter").value;
@@ -43,43 +54,74 @@
 
             //call 
             change('heightwidth');
+            updateCss();
         }
             if(parameter === 'heightwidth'){
+                
                 document.getElementById("heightRangeCounter").value = document.getElementById("heightWidthRangeCounter").value;
                 document.getElementById("widthRangeCounter").value = document.getElementById("heightWidthRangeCounter").value;
-                
+                updateCss();
 
             }
            if(parameter === 'height'){
+            
                document.getElementById("hightValue").innerText = document.getElementById("heightRangeCounter").value;
                document.getElementById("makeBox").style.height = document.getElementById("heightRangeCounter").value+"px";
+               updateCss();
            }
            if(parameter === 'width'){
+            
                document.getElementById("widthValue").innerText = document.getElementById("widthRangeCounter").value;
 
                document.getElementById("makeBox").style.width = document.getElementById("widthRangeCounter").value+"px";
+               updateCss();
                
 
            }
            if(parameter === 'border'){
+            
                document.getElementById("borderValue").innerText = document.getElementById("borderRangeCounter").value;
 
                document.getElementById("makeBox").style.border = document.getElementById("borderRangeCounter").value+"px solid";
+               updateCss();
 
            }
 
 
            if(parameter === 'border radius'){
+            
                document.getElementById("borderRadiusValue").innerText = document.getElementById("borderRadiusRangeCounter").value;
 
                document.getElementById("makeBox").style.borderRadius = document.getElementById("borderRadiusRangeCounter").value+"px";
+               updateCss();
 
            }
 
        }
        setValue();
+       updateCss();
+         
+       function updateCss(){
+        var boxStyle = document.getElementsByClassName('makeBox')[0].getAttribute('style');
+        var seperateStyle = boxStyle.split(';')
+        seperateStyle.pop(seperateStyle.length-1);
+        var inrHtml = '';
+        for (let i = 0; i < seperateStyle.length; i++) {
+            if(i == seperateStyle.length-1){
+                inrHtml+=seperateStyle[i]+';';
 
-      
+            }else{
+                inrHtml+=seperateStyle[i]+';<br>';
+            }
+        }
+        document.getElementById('cssSec').innerHTML = "{<br>"+inrHtml+"<br>}";
+
+    }
+    
+ 
+
+
+       
 
 
 
